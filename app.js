c35d555f7aeb20738a088c785a4daf45bcf7b77e
Jake Matlick <jmatlick@apple.com>
2020-03-31 20:59:09 -0700

    Renamed index.js to zulit.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/src'))
 app.set('view engine', 'pug')
-app.get('/zulit', (req, res) => res.render('index'))
+app.get('/zulit', (req, res) => res.render('zulit'))
 
 app.listen(PORT, () => console.log('App listening on port ' + PORT))
