fa06f4921dca8c2e88a2cc38212c43950fcff9a2
Jake Matlick <jmatlick@apple.com>
2020-03-31 20:48:01 -0700

    Fixed second typo

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname + '/src'))
 app.set('view engine', 'pug')
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(PORT, () => console.log('App listening on port ' + port))
+app.listen(PORT, () => console.log('App listening on port ' + PORT))
